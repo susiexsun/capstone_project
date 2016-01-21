c59211ee1e5ea697103ab46c5dee9877559f0543
susiexsun <susiexsun@gmail.com>
2016-01-20 18:07:03 -0800

    Update twitter_user.py

--- data/twitter_user.py
+++ data/twitter_user.py
@@ -6,10 +6,10 @@ import os
 import json
 import time
 
-consumer_key = "HXJfoPTVxH6Iqqzv4nY5SlYgO"
-consumer_secret = "7LfAfz2a0LmH4dH46X4mUXSH6RTVmiS9zE1kgcBkw5NPirEkJ1"
-access_token = "34633790-ENRADvdaiEsSEhudIrNRQrxZPrPcb4hMLPupf5seb"
-access_token_secret = "hCKKwx5FyzpspWvnVr1wv4sU7JHlUNwVdJPPFrcL34wT3"
+consumer_key = XXXX
+consumer_secret = XXXX
+access_token = XXXX
+access_token_secret = XXXX
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
