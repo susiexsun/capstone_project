2777e4947b5ce62b6b74a6340dc8f95218c7fdcb
susiexsun <susiexsun@gmail.com>
2016-01-20 18:06:41 -0800

    Update twitter_streaming.py

--- data/twitter_streaming.py
+++ data/twitter_streaming.py
@@ -5,10 +5,10 @@ from pymongo import MongoClient
 import os
 import json
 
-consumer_key = "HXJfoPTVxH6Iqqzv4nY5SlYgO"
-consumer_secret = "7LfAfz2a0LmH4dH46X4mUXSH6RTVmiS9zE1kgcBkw5NPirEkJ1"
-access_token = "34633790-ENRADvdaiEsSEhudIrNRQrxZPrPcb4hMLPupf5seb"
-access_token_secret = "hCKKwx5FyzpspWvnVr1wv4sU7JHlUNwVdJPPFrcL34wT3"
+consumer_key = XXXX
+consumer_secret = XXXX
+access_token = XXXX
+access_token_secret = XXXX
 
 
 class Listener(StreamListener): 
